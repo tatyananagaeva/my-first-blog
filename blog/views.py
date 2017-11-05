f83c1c2a16b6fc2d38ef87b6b675cf47a2dbd26b
tanula <tanula_04@hotmail.com>
2017-11-05 13:15:22 -0800

    Change the HTML for hte site

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import os
 
 # Create your views here.
+def post_list(request):
+	print (os.getcwd())
+	return render(request, 'blog/post_list.html')
